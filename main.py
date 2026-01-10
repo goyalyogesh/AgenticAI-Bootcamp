7c15dc0c50fcd9ee47b1bb921890efa52bb2860a
ygoyal <yogeshgoyal1989@gmail.com>
2026-01-10 15:08:40 -0500

    Add Gradio interface for Google and OpenAI models

--- main.py
+++ main.py
@@ -1,28 +1,29 @@
 from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
 from langchain_google_genai import ChatGoogleGenerativeAI
+import gradio as gr
 
 load_dotenv()
 
 llm_openai = ChatOpenAI(model="gpt-4.1-nano", temperature=0)
 llm_google = ChatGoogleGenerativeAI(model="gemini-2.5-flash", temperature=0)
 
-m1 = "Who is the President of India?"
+def askGoogle(m1):
+    response = llm_google.invoke(m1)
+    return response.content
 
-response_openai  = llm_openai.invoke(m1)
-response_google = llm_google.invoke(m1)
+def askOpenAI(m1):
+    response = llm_openai.invoke(m1)
+    return response.content
 
-print("Question1: ", m1)
+# http://127.0.0.1:7860
+demo = gr.Interface(
+    fn=askGoogle,
+    inputs=gr.Textbox(label="Ask Your question here"),
+    outputs=gr.Textbox(label="Response"),
+    title="Chat with Gemini",
+    description="Chat with Google's Gemini model"
+)
 
-print("OpenAI Response: ", response_openai.content)
-print("Google Response: ", response_google.content)
-
-m2 = "what is his age ?"
-
-response_openai_m2  = llm_openai.invoke(m2)
-response_google_m2 = llm_google.invoke(m2)
-
-print("Question2: ", m2)
-
-print("OpenAI Response: ", response_openai_m2.content)
-print("Google Response: ", response_google_m2.content)
+if __name__ == "__main__":
+    demo.launch()
